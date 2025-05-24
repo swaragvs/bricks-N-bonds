6ca4b1b3f00e17b24fa3bd553957bda3d4db9fb7
Swarag <swaragvs09@gmail.com>
2025-05-24 20:36:48 +0530

    Modified App UI
    '

--- notebook/src/App.js
+++ notebook/src/App.js
@@ -1,26 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div
       style={{
-        width: '97vw',
-        height: '90vh',
+        width: '100vw',
+        height: '100vh',
         backgroundColor: 'black',
         border: '2px solid green',
-        margin: '5vh auto',
+        margin: '0vh auto',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
       }}
     >
-      <h1 style={{ fontFamily: 'Google Sans, sans-serif', fontSize: '3rem', color: 'white' }}>
+      <h1 style={{ fontFamily: 'Google Sans, sans-serif', fontSize: '3rem', color: 'rgb(0, 255, 221)' }}>
         Notebook
       </h1>
-      <input type="text" placeholder="Search box" />
-      <button>Go</button>
+      <input className="open-book-input" type="text" placeholder="Open Book !" />
+      <button className='go-button'>Go</button>
     </div>
   );
 }
